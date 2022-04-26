9126fe7fa54624b2b9029c2534ff4b1f329f1f2a
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-04-26 19:24:11 +0530

    empty implementstion
    empty implementstion

--- hooks/src/app/app.module.ts
+++ hooks/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SongsComponent } from './songs/songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    SongsComponent
   ],
   imports: [
     BrowserModule,
