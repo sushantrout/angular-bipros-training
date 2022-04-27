98e8af1c553174ca81595db276056b22ca6faf67
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-04-27 11:29:12 +0530

    hooks songs server side implemneted
    hooks songs server side implemneted

---
+++ hooks-server/src/main/java/com/hooks/HooksServerApplication.java
@@ -0,0 +1,13 @@
+package com.hooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooksServerApplication.class, args);
+	}
+
+}
---
+++ hooks-server/src/test/java/com/hooks/HooksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
